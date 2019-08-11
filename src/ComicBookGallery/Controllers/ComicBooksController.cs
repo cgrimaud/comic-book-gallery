061c7791c48b558378407a2c91512872a3ed95ac
Christy Grimaud <christy.grimaud@gmail.com>
2019-08-11 13:41:31 -0400

    Updated Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -16,6 +16,20 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
+            // ViewBag is an object provided by MVC that allows us to pass data from a controller to a view
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
             return View();
         } 
     }
